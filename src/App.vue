24ba49bb0b5f7e7a957fc052c5ee93dca4f214ec
manuelgeek <emashmagak@appslab.co.ke>
2020-08-13 19:40:15 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <button @click="toggleModalState">Open modal</button>
+  <modal v-if="modalOpen">
+    <p>Hello, I'm a modal window.</p>
+  </modal>
+</template>
+
+<script>
+
+import Modal from "./Modal";
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data: () => ({
+    modalOpen: true
+  }),
+  methods: {
+    toggleModalState() {
+      this.modalOpen = !this.modalOpen;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
